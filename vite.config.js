abe5a13b098cba87aac1637966a702fa300ab2cd
Jim Jiang <jim@lotlab.org>
2024-11-24 20:36:06 +0800

    feat: 新增维护公告页面

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import fs from "fs";
+
+const pages = Object.fromEntries(
+  fs.readdirSync("./pages").map((item) => [
+    item,
+    path.resolve(`${__dirname}/pages/${item}/index.html`),
+  ])
+);
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ...pages
+      },
+    },
+  },
+})
